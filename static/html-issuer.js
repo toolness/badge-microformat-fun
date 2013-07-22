a750808d940cebc2d0cb2badc577b4243396d43e
Atul Varma <varmaa@gmail.com>
2013-07-21 20:20:15 -0400

    added html-issuer.js.

---
+++ static/html-issuer.js
@@ -0,0 +1,26 @@
+(function() {
+  var me = document.querySelector('script[src$="/html-issuer.js"]');
+  var myOrigin = me.src.match(/^(.*)\/html-issuer.js/)[1];
+
+  function getClosestAncestor(element, className) {
+    if (!element) return null;
+    if (element.classList.contains(className)) return element;
+    return getClosestAncestor(element.parentElement, className);
+  }
+
+  window.addEventListener("click", function(event) {
+    if (event.target.hasAttribute('data-issue-html-badge')) {
+      var hBadge = getClosestAncestor(event.target, 'h-badge');
+      if (hBadge) {
+        var id = hBadge.getAttribute('id') || '';
+        var pageURL = location.protocol + '//' +
+                      location.host + location.pathname +
+                      location.search + (id ? '#' + id : '');
+        var assertionURL = myOrigin + '/assertion?url=' + 
+                           encodeURIComponent(pageURL);
+        OpenBadges.issue([assertionURL]);
+        event.preventDefault();
+      }
+    }
+  }, true);
+})();
